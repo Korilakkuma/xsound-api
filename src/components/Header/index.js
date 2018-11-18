a9366b0f97d3efde2edeb6849a42bab0d06183d7
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2018-11-18 13:44:11 +0900

    Add GitHub ribbon

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -43,6 +43,13 @@ class Header extends React.Component {
     render() {
         return (
             <header className={Header.CLASS_NAME}>
+                <a href="https://github.com/Korilakkuma/XSound">
+                    <img
+                      style={{ position : 'absolute', top : '0', right : '0', border : '0' }}
+                      src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png"
+                      alt="Fork me on GitHub"
+                    />
+                </a>
                 <div className={`${Header.CLASS_NAME}__headings`}>
                     <h1 className={`${Header.CLASS_NAME}__title`}>XSound</h1>
                     <h2 className={`${Header.CLASS_NAME}__subtitle`}>Web Audio API Library</h2>
