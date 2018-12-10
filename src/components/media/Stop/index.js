8a0ed8cf668fa171de79cdcc9555183e45741cbc
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2018-12-10 22:00:00 +0900

    Update `MediaModule#stop`

--- src/components/media/Stop/index.js
+++ src/components/media/Stop/index.js
@@ -27,6 +27,26 @@ class Stop extends React.Component {
                     <p>
                         This method stops media.
                     </p>
+                    <table>
+                        <caption>Arguments</caption>
+                        <thead><tr><th scope="col"></th><th scope="col">Type</th><th scope="col">Description</th></tr></thead>
+                        <tbody>
+                            <tr>
+                                <th scope="row">1st</th>
+                                <td><span className="argument-type">Function</span></td>
+                                <td>
+                                    This argument is function that is invoked on success.
+                                </td>
+                            </tr>
+                            <tr>
+                                <th scope="row">2nd</th>
+                                <td><span className="argument-type">Function</span></td>
+                                <td>
+                                    This argument is function that is invoked on error.
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </section>
                 <section className="codepen">
                     <iframe height='265' scrolling='no' title='MediaModule#stop | XSound - Web Audio API Library -' src='//codepen.io/Korilakkuma/embed/VXLeYq/?height=265&amp;theme-id=0&amp;default-tab=js,result&amp;embed-version=2' frameBorder='no' allowtransparency='true' allowFullScreen='true' style={{ width : '100%' }}>See the Pen <a href='https://codepen.io/Korilakkuma/pen/VXLeYq/'>MediaModule#stop | XSound - Web Audio API Library -</a> by Tomohiro IKEDA (<a href='https://codepen.io/Korilakkuma'>@Korilakkuma</a>) on <a href='https://codepen.io'>CodePen</a>.
