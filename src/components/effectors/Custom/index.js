975e1d903b18e95f3804b9d6000fbd4572918eca
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2019-07-19 17:27:00 +0900

    Add `section`

--- src/components/effectors/Custom/index.js
+++ src/components/effectors/Custom/index.js
@@ -27,19 +27,20 @@ class Custom extends React.Component {
                     <p>
                         This module is implemented by this library user.
                     </p>
-                    <p>
-                        <ol>
-                            <li>
-                                1. Extends <code>XSound.Effector</code>
-                                <pre><code>{`class GraphicEqualizer extends X.Effector {
+                    <section>
+                        <p>
+                            <ol>
+                                <li>
+                                    1. Extends <code>XSound.Effector</code>
+                                    <pre><code>{`class GraphicEqualizer extends X.Effector {
     constructor(context) {
         super(context);
     }
 }`}</code></pre>
-                            </li>
-                            <li>
-                                2. Override some methods (<code>param</code>, <code>start</code>, <code>stop</code> ... etc)
-                                <pre><code>{`class GraphicEqualizer extends X.Effector {
+                                </li>
+                                <li>
+                                    2. Override some methods (<code>param</code>, <code>start</code>, <code>stop</code> ... etc)
+                                    <pre><code>{`class GraphicEqualizer extends X.Effector {
     constructor(context) {
         super(context);
     }
@@ -59,13 +60,14 @@ class Custom extends React.Component {
         // ...
     }
 }`}</code></pre>
-                            </li>
-                            <li>
-                                3. Invoke <code>install</code> method in target <code>SoundModule</code>
-                                <pre><code>X('audio').install('graphicequalizer', new GraphicEqualizer(X.get()));</code></pre>
-                            </li>
-                         </ol>
-                     </p>
+                                </li>
+                                <li>
+                                    3. Invoke <code>install</code> method in target <code>SoundModule</code>
+                                    <pre><code>X('audio').install('graphicequalizer', new GraphicEqualizer(X.get()));</code></pre>
+                                </li>
+                             </ol>
+                         </p>
+                     </section>
                 </section>
                 <section className="codepen">
                     <iframe height='265' scrolling='no' title='Custom | XSound - Web Audio API Library -' src='//codepen.io/Korilakkuma/embed/BMLRjM/?height=265&amp;theme-id=0&amp;default-tab=js,result&amp;embed-version=2' frameBorder='no' allowtransparency='true' allowFullScreen='true' style={{ width : '100%' }}>See the Pen <a href='https://codepen.io/Korilakkuma/pen/BMLRjM/'>Custom | XSound - Web Audio API Library -</a> by Tomohiro IKEDA (<a href='https://codepen.io/Korilakkuma'>@Korilakkuma</a>) on <a href='https://codepen.io'>CodePen</a>.
