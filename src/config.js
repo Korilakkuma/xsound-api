af5ace91edb588d87e18743fe715bac966d96dbe
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2020-09-20 17:02:00 +0900

    Add config file

---
+++ src/config.js
@@ -0,0 +1,2 @@
+export const BASS_PATH = '/';  // location.pathname
+export const TITLE = 'XSound - Web Audio API Library for Synthesizer, Effects, Visualization, Multi-Track Recording, Audio Streaming, Visual Audio Sprite ... -';
