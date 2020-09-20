7c3d2179b272dfba13c0b7a00e8dae1d2389763c
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2020-09-20 17:27:12 +0900

    Add component for code viewer

---
+++ src/components/utils/codeviewer.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export const CodeViewer = function(props) {
+  const { title, path } = props;
+
+  return (
+    <section className="codepen">
+      <iframe
+        height='265'
+        scrolling='no'
+        title={`${title} | XSound - Web Audio API Library -`}
+        src={`https://codepen.io/Korilakkuma/embed/${path}?height=265&amp;theme-id=dark&amp;default-tab=js,result`}
+        frameBorder='no'
+        loading='lazy'
+        allowtransparency='true'
+        allowFullScreen={true}
+        style={{ width: '100%' }}
+      >
+        See the Pen
+        <a href={`https://codepen.io/Korilakkuma/pen/${path}/`}>${title} | XSound - Web Audio API Library -</a>
+        by Tomohiro IKEDA (<a href='https://codepen.io/Korilakkuma'>@Korilakkuma</a>) on <a href='https://codepen.io'>CodePen</a>.
+      </iframe>
+    </section>
+  );
+};
+
+CodeViewer.propTypes = {
+  title: PropTypes.string.isRequired,
+  path : PropTypes.string.isRequired
+};
