b3c0f1295815aae3a626bae08d8f47c0a7bc77ba
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2020-09-22 17:27:12 +0900

    Refactor component for page loading bar

--- src/components/PageLoadingBar/index.js
+++ src/components/PageLoadingBar/index.js
@@ -1,27 +1,20 @@
-'use strict';
-
 import React from 'react';
-import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
 class PageLoadingBar extends React.Component {
-    static CLASS_NAME  = 'PageLoadingBar';
+  static CLASS_NAME  = 'PageLoadingBar';
 
-    constructor(props) {
-        super(props);
-    }
+  shouldComponentUpdate() {
+    return false;
+  }
 
-    shouldComponentUpdate() {
-        return false;
-    }
-
-    render() {
-        return (
-            <div id="page-loading-bar" className={PageLoadingBar.CLASS_NAME}>
-                <div role="progressbar" />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div id="page-loading-bar" className={PageLoadingBar.CLASS_NAME}>
+        <div role="progressbar" />
+      </div>
+    );
+  }
 }
 
 export default connect()(PageLoadingBar);
