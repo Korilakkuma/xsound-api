c6125ecfb0098f90cb0fb7e723819625bd7c566c
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2020-09-22 23:12:00 +0900

    Refactor component for footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,26 +1,20 @@
-'use strict';
-
 import React from 'react';
 import { connect } from 'react-redux';
 
 class Footer extends React.Component {
-    static CLASS_NAME = 'Footer';
+  static CLASS_NAME = 'Footer';
 
-    constructor(props) {
-        super(props);
-    }
+  shouldComponentUpdate() {
+    return false;
+  }
 
-    shouldComponentUpdate() {
-        return false;
-    }
-
-    render() {
-        return (
-            <footer className={Footer.CLASS_NAME}>
-                <p>XSound maintained by <a href="https://github.com/Korilakkuma" target="_blank" rel="noopener noreferrer">Korilakkuma</a></p>
-            </footer>
-        );
-    }
+  render() {
+    return (
+      <footer className={Footer.CLASS_NAME}>
+        <p>XSound maintained by <a href="https://github.com/Korilakkuma" target="_blank" rel="noopener noreferrer">Korilakkuma</a></p>
+      </footer>
+    );
+  }
 }
 
 export default connect()(Footer);
