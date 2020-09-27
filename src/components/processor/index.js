26c8f28f1362eb8181571eedc6cb58e5ac33ace8
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2020-09-27 23:12:00 +0900

    Refactor components for `ProcessorModule`

--- src/components/processor/index.js
+++ src/components/processor/index.js
@@ -1,11 +1,9 @@
-'use strict';
-
 import Setup from './Setup';
 import Start from './Start';
 import Stop from './Stop';
 
 export {
-    Setup,
-    Start,
-    Stop
+  Setup,
+  Start,
+  Stop
 };
