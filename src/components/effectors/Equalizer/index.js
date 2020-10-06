1184c0fd070a2a51c4fa7a543afea8d3f8ff2023
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2020-10-06 17:27:12 +0900

    Refactor component for `Equalizer`

--- src/components/effectors/Equalizer/index.js
+++ src/components/effectors/Equalizer/index.js
@@ -1,70 +1,62 @@
-'use strict';
-
 import React from 'react';
 import { connect } from 'react-redux';
+import { CodeViewer } from '../../utils/codeviewer';
 
 class Equalizer extends React.Component {
-    static TITLE      = 'Equalizer';
-    static CLASS_NAME = 'Equalizer';
+  static TITLE      = 'Equalizer';
+  static CLASS_NAME = 'Equalizer';
 
-    constructor(props) {
-        super(props);
-    }
+  shouldComponentUpdate() {
+    return false;
+  }
 
-    shouldComponentUpdate() {
-        return false;
-    }
-
-    render() {
-        return (
-            <main className={Equalizer.CLASS_NAME}>
-                <section>
-                    <div className="component-title">
-                        <h1>equalizer</h1>
-                        <p className="applicable">Applicable : <span>*</span></p>
-                    </div>
-                    <hr role="presentation" />
-                    <p>
-                        This module is Equalizer.
-                    </p>
-                    <table>
-                        <caption>Parameters</caption>
-                        <thead><tr><th scope="col"></th><th scope="col">Type</th><th scope="col">Value</th><th scope="col">Default</th></tr></thead>
-                        <tbody>
-                            <tr>
-                                <th scope="row">bass</th>
-                                <td><span className="param-type">number</span></td>
-                                <td>-40 &lt;= value &lt;= 40</td>
-                                <td>0</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">middle</th>
-                                <td><span className="param-type">number</span></td>
-                                <td>-40 &lt;= value &lt;= 40</td>
-                                <td>0</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">treble</th>
-                                <td><span className="param-type">number</span></td>
-                                <td>-40 &lt;= value &lt;= 40</td>
-                                <td>0</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">presence</th>
-                                <td><span className="param-type">number</span></td>
-                                <td>-40 &lt;= value &lt;= 40</td>
-                                <td>0</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </section>
-                <section className="codepen">
-                    <iframe height='265' scrolling='no' title='Equalizer | XSound - Web Audio API Library -' src='//codepen.io/Korilakkuma/embed/YLXrgR/?height=265&amp;theme-id=0&amp;default-tab=js,result&amp;embed-version=2' frameBorder='no' allowtransparency='true' allowFullScreen='true' style={{ width : '100%' }}>See the Pen <a href='https://codepen.io/Korilakkuma/pen/YLXrgR/'>Equalizer | XSound - Web Audio API Library -</a> by Tomohiro IKEDA (<a href='https://codepen.io/Korilakkuma'>@Korilakkuma</a>) on <a href='https://codepen.io'>CodePen</a>.
-</iframe>
-                </section>
-            </main>
-        );
-    }
+  render() {
+    return (
+      <main className={Equalizer.CLASS_NAME}>
+        <section>
+          <div className="component-title">
+            <h1>equalizer</h1>
+            <p className="applicable">Applicable : <span>*</span></p>
+          </div>
+          <hr role="presentation" />
+          <p>
+            This module is Equalizer.
+          </p>
+          <table>
+            <caption>Parameters</caption>
+            <thead><tr><th scope="col"></th><th scope="col">Type</th><th scope="col">Value</th><th scope="col">Default</th></tr></thead>
+            <tbody>
+              <tr>
+                <th scope="row">bass</th>
+                <td><span className="param-type">number</span></td>
+                <td>-40 &lt;= value &lt;= 40</td>
+                <td>0</td>
+              </tr>
+              <tr>
+                <th scope="row">middle</th>
+                <td><span className="param-type">number</span></td>
+                <td>-40 &lt;= value &lt;= 40</td>
+                <td>0</td>
+              </tr>
+              <tr>
+                <th scope="row">treble</th>
+                <td><span className="param-type">number</span></td>
+                <td>-40 &lt;= value &lt;= 40</td>
+                <td>0</td>
+              </tr>
+              <tr>
+                <th scope="row">presence</th>
+                <td><span className="param-type">number</span></td>
+                <td>-40 &lt;= value &lt;= 40</td>
+                <td>0</td>
+              </tr>
+            </tbody>
+          </table>
+        </section>
+        <CodeViewer title='Equalizer' path='YLXrgR' />
+      </main>
+    );
+  }
 }
 
 export default connect()(Equalizer);
