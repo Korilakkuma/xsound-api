59bb998e7c82d79da61d7340ad179064b09aea18
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-02-13 23:12:00 +0900

    Fix layouts component for overview
    - Fix text
    - Fix `propTypes`
    - Fix styles

--- src/components/layouts/Overview/index.js
+++ src/components/layouts/Overview/index.js
@@ -9,7 +9,7 @@ export const Overview = (props) => {
       <div className="Overview__heading">
         <h1>{title}</h1>
         {Array.isArray(returnTypes) ? <p className="Overview__returns">Returns : <span>{returnTypes.join(' | ')}</span></p> : null}
-        {Array.isArray(applicableModules) ? <p className="Overview__applicable">Returns : <span>{applicableModules.join(' | ')}</span></p> : null}
+        {Array.isArray(applicableModules) ? <p className="Overview__applicable">Applicable : <span>{applicableModules.join(' | ')}</span></p> : null}
       </div>
       <hr role="presentation" />
       <p>{description}</p>
@@ -18,8 +18,8 @@ export const Overview = (props) => {
 };
 
 Overview.propTypes = {
-  title            : PropTypes.string.required,
-  returnTypes      : PropTypes.array,
-  applicableModules: PropTypes.array,
-  description      : PropTypes.string.required
+  title            : PropTypes.string.isRequired,
+  returnTypes      : PropTypes.arrayOf(PropTypes.string),
+  applicableModules: PropTypes.arrayOf(PropTypes.string),
+  description      : PropTypes.string.isRequired
 };
