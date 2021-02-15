7eae28c33258a4919db70486006e2886243d436a
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-02-15 23:53:38 +0900

    Refactor `XSound.ajax` component by using layout components

--- src/components/xsound/Ajax/index.js
+++ src/components/xsound/Ajax/index.js
@@ -1,59 +1,45 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { Overview } from '../../layouts/Overview';
+import { Arguments } from '../../layouts/Arguments';
 import { CodeViewer } from '../../layouts/CodeViewer';
 
-class Ajax extends React.Component {
-  static TITLE      = 'XSound.ajax';
-  static CLASS_NAME = 'Ajax';
+const CLASS_NAME = 'Ajax';
 
-  shouldComponentUpdate() {
-    return false;
-  }
+export const Ajax = () => {
+  return (
+    <main className={CLASS_NAME}>
+      <Overview
+        title='ajax'
+        returnTypes={['undefined']}
+        description='This method gets the instance of ArrayBuffer by Ajax.'
+      />
+      <Arguments
+        rows={[
+          {
+            types      : ['Object'],
+            description: `
+              This argument is plain object that has the following properties.
+              <dl class="list-marker">
+                <dt>url<span class="Arguments__type">string</span></dt>
+                <dd>This value is resource URL.</dd>
+                <dt>type<span class="Arguments__type">string</span></dt>
+                <dd>This value is response type that is one of &apos;text&apos;, &apos;arraybuffer&apos;, &apos;blob&apos;, &apos;document&apos;, &apos;json&apos;. The default value is &apos;arraybuffer&apos;.</dd>
+                <dt>timeout<span class="Arguments__type">number</span></dt>
+                <dd>This value is Ajax timeout.</dd>
+                <dt>success<span class="Arguments__type">Function</span></dt>
+                <dd>This function is invoked on success.</dd>
+                <dt>error<span class="Arguments__type">Function</span></dt>
+                <dd>This function is invoked on error.</dd>
+                <dt>progress<span class="Arguments__type">Function</span></dt>
+                <dd>This function is invoked during connection.</dd>
+              </dl>
+            `
+          }
+        ]}
+      />
+      <CodeViewer title='ajax' path='PExBqJ' />
+    </main>
+  );
+};
 
-  render() {
-    return (
-      <main className={Ajax.CLASS_NAME}>
-        <section>
-          <div className="component-title">
-            <h1>ajax</h1>
-            <p className="returns">Returns : <span>undefined</span></p>
-          </div>
-          <hr role="presentation" />
-          <p>
-            This method gets the instance of ArrayBuffer by <abbr title="Asynchronous JavaScript and XML">Ajax</abbr>.
-          </p>
-        </section>
-        <table>
-          <caption>Arguments</caption>
-          <thead><tr><th scope="col"></th><th scope="col">Type</th><th scope="col">Description</th></tr></thead>
-          <tbody>
-            <tr>
-              <th scope="row">1st</th>
-              <td><span className="argument-type">Object</span></td>
-              <td>
-                This argument is plain object that has the following properties.
-                <dl className="list-marker">
-                  <dt>url<span className="argument-type">string</span></dt>
-                  <dd>This value is resource URL.</dd>
-                  <dt>type<span className="argument-type">string</span></dt>
-                  <dd>This value is response type that is one of &apos;text&apos;, &apos;arraybuffer&apos;, &apos;blob&apos;, &apos;document&apos;, &apos;json&apos;. The default value is &apos;arraybuffer&apos;.</dd>
-                  <dt>timeout<span className="argument-type">number</span></dt>
-                  <dd>This value is <abbr title="Asynchronous JavaScript and XML">Ajax</abbr> timeout.</dd>
-                  <dt>success<span className="argument-type">Function</span></dt>
-                  <dd>This function is invoked on success.</dd>
-                  <dt>error<span className="argument-type">Function</span></dt>
-                  <dd>This function is invoked on error.</dd>
-                  <dt>progress<span className="argument-type">Function</span></dt>
-                  <dd>This function is invoked during connection.</dd>
-                </dl>
-              </td>
-            </tr>
-          </tbody>
-        </table>
-        <CodeViewer title='ajax' path='PExBqJ' />
-      </main>
-    );
-  }
-}
-
-export default connect()(Ajax);
+Ajax.TITLE = 'XSound.ajax';
--- src/components/xsound/index.js
+++ src/components/xsound/index.js
@@ -3,7 +3,7 @@ import SampleRate from './SampleRate';
 import NumberOfInputs from './NumberOfInputs';
 import NumberOfOutputs from './NumberOfOutputs';
 import FFT from './FFT';
-import Ajax from './Ajax';
+import { Ajax } from './Ajax';
 import Clone from './Clone';
 import ConvertTime from './ConvertTime';
 import Decode from './Decode';
