406fdbff11a118d5452429313d56018aec4b8848
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-03-16 20:00:00 +0900

    Correspond to property

--- src/components/layouts/Overview/index.js
+++ src/components/layouts/Overview/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export const Overview = (props) => {
-  const { title, returnTypes, applicableModules, description } = props;
+  const { title, types, returnTypes, applicableModules, description } = props;
 
   return (
     <section className="Overview">
       <div className="Overview__heading">
         <h1>{title}</h1>
+        {Array.isArray(types) ? <p className="Overview__types">Types : <span>{types.join(' | ')}</span></p> : null}
         {Array.isArray(returnTypes) ? <p className="Overview__returns">Returns : <span>{returnTypes.join(' | ')}</span></p> : null}
         {Array.isArray(applicableModules) ? <p className="Overview__applicable">Applicable : <span>{applicableModules.join(' | ')}</span></p> : null}
       </div>
@@ -19,6 +20,7 @@ export const Overview = (props) => {
 
 Overview.propTypes = {
   title            : PropTypes.string.isRequired,
+  types            : PropTypes.arrayOf(PropTypes.string),
   returnTypes      : PropTypes.arrayOf(PropTypes.string),
   applicableModules: PropTypes.arrayOf(PropTypes.string),
   description      : PropTypes.string.isRequired
