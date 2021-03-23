fd953d35da65847618d89ac851d593c71dbc844e
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-03-24 08:52:21 +0900

    Refactor `OscillatorModule#stop` component by using layout components

--- src/components/oscillator/Stop/index.js
+++ src/components/oscillator/Stop/index.js
@@ -1,43 +1,20 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { Overview } from '../../layouts/Overview';
 import { CodeViewer } from '../../layouts/CodeViewer';
 
-class Stop extends React.Component {
-  static TITLE      = 'OscillatorModule#stop';
-  static CLASS_NAME = 'Stop';
+const CLASS_NAME = 'OscillatorModule#Stop';
 
-  shouldComponentUpdate() {
-    return false;
-  }
+export const Stop = () => {
+  return (
+    <main className={CLASS_NAME}>
+      <Overview
+        title='stop'
+        returnTypes={['OscillatorModule']}
+        description='This method stops sound.'
+      />
+      <CodeViewer title='OscillatorModule#stop' path='EQNgJG' />
+    </main>
+  );
+};
 
-  render() {
-    return (
-      <main className={Stop.CLASS_NAME}>
-        <section>
-          <div className="component-title">
-            <h1>stop</h1>
-            <p className="returns">Returns : <span>OscillatorModule</span></p>
-          </div>
-          <hr role="presentation" />
-          <p>
-            This method stops sound.
-          </p>
-          <table>
-            <caption>Arguments</caption>
-            <thead><tr><th scope="col"></th><th scope="col">Type</th><th scope="col">Description</th></tr></thead>
-            <tbody>
-              <tr>
-                <th scope="row">1st</th>
-                <td><span className="argument-type">Function</span></td>
-                <td>This value is <a href="http://webaudio.github.io/web-audio-api/#the-scriptprocessornode-interface---deprecated" target="_blank" rel="noopener noreferrer nofollow">onaudioprocess</a> event hanlder.</td>
-              </tr>
-            </tbody>
-          </table>
-        </section>
-        <CodeViewer title='OscillatorModule#stop' path='EQNgJG' />
-      </main>
-    );
-  }
-}
-
-export default connect()(Stop);
+Stop.TITLE = 'OscillatorModule#stop';
--- src/components/oscillator/index.js
+++ src/components/oscillator/index.js
@@ -3,7 +3,7 @@ import { Oscillator } from './Oscillator';
 import { Setup } from './Setup';
 import { Ready } from './Ready';
 import { Start } from './Start';
-import Stop from './Stop';
+import { Stop } from './Stop';
 import Param from './Param';
 import Params from './Params';
 import ToJSON from './ToJSON';
