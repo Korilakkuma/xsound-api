640969fe9b863822b3ffe6b7205b9b6c3be9f87f
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-04-05 19:25:02 +0900

    Refactor `OneshotModule#params` component by using layout components

--- src/components/oneshot/Params/index.js
+++ src/components/oneshot/Params/index.js
@@ -1,32 +1,20 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { Overview } from '../../layouts/Overview';
 import { CodeViewer } from '../../layouts/CodeViewer';
 
-class Params extends React.Component {
-  static TITLE      = 'OneshotModule#params';
-  static CLASS_NAME = 'Params';
+const CLASS_NAME = 'OneshotModule#Params';
 
-  shouldComponentUpdate() {
-    return false;
-  }
+export const Params = () => {
+  return (
+    <main className={CLASS_NAME}>
+      <Overview
+        title='params'
+        returnTypes={['Object']}
+        description='This method gets the associative array for parameters.'
+      />
+      <CodeViewer title='OneshotModule#params' path='BYOQLE' />
+    </main>
+  );
+};
 
-  render() {
-    return (
-      <main className={Params.CLASS_NAME}>
-        <section>
-          <div className="component-title">
-            <h1>params</h1>
-            <p className="returns">Returns : <span>Object</span></p>
-          </div>
-          <hr role="presentation" />
-          <p>
-            This method gets the associative array for parameters.
-          </p>
-        </section>
-        <CodeViewer title='OneshotModule#params' path='BYOQLE' />
-      </main>
-    );
-  }
-}
-
-export default connect()(Params);
+Params.TITLE = 'OneshotModule#params';
--- src/components/oneshot/index.js
+++ src/components/oneshot/index.js
@@ -4,7 +4,7 @@ import { Ready } from './Ready';
 import { Start } from './Start';
 import { Stop } from './Stop';
 import { Param } from './Param';
-import Params from './Params';
+import { Params } from './Params';
 import ToJSON from './ToJSON';
 import Get from './Get';
 
