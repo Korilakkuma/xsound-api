263161dfb13f0b82d8fd76a97e4aa4ae711b1638
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-04-08 23:12:00 +0900

    Refactor `AudioModule#params` component by using layout components

--- src/components/audio/Params/index.js
+++ src/components/audio/Params/index.js
@@ -1,32 +1,20 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { Overview } from '../../layouts/Overview';
 import { CodeViewer } from '../../layouts/CodeViewer';
 
-class Params extends React.Component {
-  static TITLE      = 'AudioModule#params';
-  static CLASS_NAME = 'Params';
+const CLASS_NAME = 'AudioModule#Params';
 
-  shouldComponentUpdate() {
-    return false;
-  }
+export const Params = () => {
+  return (
+    <main className={CLASS_NAME}>
+      <Overview
+        title='params'
+        returnTypes={['Object']}
+        description='This method gets the associative array for parameters.'
+      />
+      <CodeViewer title='AudioModule#params' path='jZRRVN' />
+    </main>
+  );
+};
 
-  render() {
-    return (
-      <main className={Params.CLASS_NAME}>
-        <section>
-          <div className="component-title">
-            <h1>params</h1>
-            <p className="returns">Returns : <span>Object</span></p>
-          </div>
-          <hr role="presentation" />
-          <p>
-            This method gets the associative array for parameters.
-          </p>
-        </section>
-        <CodeViewer title='AudioModule#params' path='jZRRVN' />
-      </main>
-    );
-  }
-}
-
-export default connect()(Params);
+Params.TITLE = 'AudioModule#params';
