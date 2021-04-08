2d1ca83c149b80aa0ac9bb8477cd8cd35ea840d9
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-04-08 20:00:00 +0900

    Refactor `AudioModule#stop` component by using layout components

--- src/components/audio/Stop/index.js
+++ src/components/audio/Stop/index.js
@@ -1,32 +1,20 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { Overview } from '../../layouts/Overview';
 import { CodeViewer } from '../../layouts/CodeViewer';
 
-class Stop extends React.Component {
-  static TITLE      = 'AudioModule#stop';
-  static CLASS_NAME = 'Stop';
+const CLASS_NAME = 'AudioModule#Stop';
 
-  shouldComponentUpdate() {
-    return false;
-  }
+export const Stop = () => {
+  return (
+    <main className={CLASS_NAME}>
+      <Overview
+        title='stop'
+        returnTypes={['AudioModule']}
+        description='This method stops audio.'
+      />
+      <CodeViewer title='AudioModule#stop' path='eVbQWz' />
+    </main>
+  );
+};
 
-  render() {
-    return (
-      <main className={Stop.CLASS_NAME}>
-        <section>
-          <div className="component-title">
-            <h1>stop</h1>
-            <p className="returns">Returns : <span>AudioModule</span></p>
-          </div>
-          <hr role="presentation" />
-          <p>
-            This method stops audio.
-          </p>
-        </section>
-        <CodeViewer title='AudioModule#stop' path='eVbQWz' />
-      </main>
-    );
-  }
-}
-
-export default connect()(Stop);
+Stop.TITLE = 'AudioModule#stop';
--- src/components/audio/index.js
+++ src/components/audio/index.js
@@ -2,7 +2,7 @@ import { AudioModule } from './AudioModule';
 import { Setup } from './Setup';
 import { Ready } from './Ready';
 import { Start } from './Start';
-import Stop from './Stop';
+import { Stop } from './Stop';
 import Param from './Param';
 import Params from './Params';
 import ToJSON from './ToJSON';
