77eacbed0e6ba4fa564024fd5f90fcede6e8f2e6
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-04-10 17:02:00 +0900

    Refactor `AudioModule#isBuffer` component by using layout components

--- src/components/audio/IsBuffer/index.js
+++ src/components/audio/IsBuffer/index.js
@@ -1,32 +1,20 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { Overview } from '../../layouts/Overview';
 import { CodeViewer } from '../../layouts/CodeViewer';
 
-class IsBuffer extends React.Component {
-  static TITLE      = 'AudioModule#isBuffer';
-  static CLASS_NAME = 'IsBuffer';
+const CLASS_NAME = 'AudioModule#IsBuffer';
 
-  shouldComponentUpdate() {
-    return false;
-  }
+export const IsBuffer = () => {
+  return (
+    <main className={CLASS_NAME}>
+      <Overview
+        title='isBuffer'
+        returnTypes={['boolean']}
+        description='This method is to check for presence or absence of AudioBuffer.'
+      />
+      <CodeViewer title='AudioModule#isBuffer' path='oERQZg' />
+    </main>
+  );
+};
 
-  render() {
-    return (
-      <main className={IsBuffer.CLASS_NAME}>
-        <section>
-          <div className="component-title">
-            <h1>isBuffer</h1>
-            <p className="returns">Returns : <span>boolean</span></p>
-          </div>
-          <hr role="presentation" />
-          <p>
-            This method is to check for presence or absence of AudioBuffer.
-          </p>
-        </section>
-        <CodeViewer title='AudioModule#isBuffer' path='oERQZg' />
-      </main>
-    );
-  }
-}
-
-export default connect()(IsBuffer);
+IsBuffer.TITLE = 'AudioModule#isBuffer';
--- src/components/audio/index.js
+++ src/components/audio/index.js
@@ -8,7 +8,7 @@ import { Params } from './Params';
 import { ToJSON } from './ToJSON';
 import { Get } from './Get';
 import { Toggle } from './Toggle';
-import IsBuffer from './IsBuffer';
+import { IsBuffer } from './IsBuffer';
 import IsSource from './IsSource';
 import IsPaused from './IsPaused';
 import FadeIn from './FadeIn';
