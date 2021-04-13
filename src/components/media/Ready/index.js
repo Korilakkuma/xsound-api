1b6a243f8550dcdf3bbb71e941f3b4f6ea50f6ad
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-04-13 20:00:00 +0900

    Refactor `MediaModule#ready` component by using layout components

--- src/components/media/Ready/index.js
+++ src/components/media/Ready/index.js
@@ -1,53 +1,34 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { Overview } from '../../layouts/Overview';
+import { Arguments } from '../../layouts/Arguments';
 import { CodeViewer } from '../../layouts/CodeViewer';
 
-class Ready extends React.Component {
-  static TITLE      = 'MediaModule#ready';
-  static CLASS_NAME = 'Ready';
+const CLASS_NAME = 'MediaModule#Ready';
 
-  shouldComponentUpdate() {
-    return false;
-  }
+export const Ready = () => {
+  return (
+    <main className={CLASS_NAME}>
+      <Overview
+        title='ready'
+        returnTypes={['MediaModule']}
+        description='This method reads media.'
+      />
+      <Arguments
+        rows={[
+          {
+            types      : ['string'],
+            description: 'This value is media URL.'
+          },
+          {
+            types      : ['string'],
+            description: 'This value is codec (ex. &apos;audio/mpeg&apos;). This value is required in the case of audio streaming.'
+          }
+        ]}
+      />
+      <CodeViewer title='MediaModule#ready' path='eMNmjK' />
+      <CodeViewer title='MediaModule#ready' path='xxKOWbv' />
+    </main>
+  );
+};
 
-  render() {
-    return (
-      <main className={Ready.CLASS_NAME}>
-        <section>
-          <div className="component-title">
-            <h1>ready</h1>
-            <p className="returns">Returns : <span>MediaModule</span></p>
-          </div>
-          <hr role="presentation" />
-          <p>
-            This method reads media.
-          </p>
-          <table>
-            <caption>Arguments</caption>
-            <thead><tr><th scope="col"></th><th scope="col">Type</th><th scope="col">Description</th></tr></thead>
-            <tbody>
-              <tr>
-                <th scope="row">1st</th>
-                <td><span className="argument-type">string</span></td>
-                <td>
-                  This value is media URL.
-                </td>
-              </tr>
-              <tr>
-                <th scope="row">2nd</th>
-                <td><span className="argument-type">string</span></td>
-                <td>
-                  This value is codec (ex. &apos;audio/mpeg&apos;). This value is required in the case of audio streaming.
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </section>
-        <CodeViewer title='MediaModule#ready' path='eMNmjK' />
-        <CodeViewer title='MediaModule#ready' path='xxKOWbv' />
-      </main>
-    );
-  }
-}
-
-export default connect()(Ready);
+Ready.TITLE = 'MediaModule#ready';
