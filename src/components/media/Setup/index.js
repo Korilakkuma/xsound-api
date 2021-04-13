3434936bf4509e14d7630bd0533c26b749dcc734
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-04-13 18:00:00 +0900

    Refactor `MediaModule#setup` component by using layout components

--- src/components/media/Setup/index.js
+++ src/components/media/Setup/index.js
@@ -1,55 +1,41 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { Overview } from '../../layouts/Overview';
+import { Arguments } from '../../layouts/Arguments';
 import { CodeViewer } from '../../layouts/CodeViewer';
 
-class Setup extends React.Component {
-  static TITLE      = 'MediaModule#setup';
-  static CLASS_NAME = 'Setup';
+const CLASS_NAME = 'MediaModule#Setup';
 
-  shouldComponentUpdate() {
-    return false;
-  }
+export const Setup = () => {
+  return (
+    <main className={CLASS_NAME}>
+      <Overview
+        title='setup'
+        returnTypes={['MediaModule']}
+        description='This method setups HTMLMediaElement, media formats and event listeners.'
+      />
+      <Arguments
+        rows={[
+          {
+            types      : ['Object'],
+            description: `
+              This argument is plain object that has the following properties.
+              <dl class="list-marker">
+                <dt>media<span class="Arguments__type">HTMLAudioElement</span> or <span class="Arguments__type">HTMLVideoElement</span></dt>
+                <dd>This value is the instance of HTMLAudioElement or HTMLVideoElement.</dd>
+                <dt>formats<span class="Arguments__type">Array</span></dt>
+                <dd>This value is the array that contains media formats.</dd>
+                <dt>listeners<span class="Arguments__type">Object</span></dt>
+                <dd>This value is the associative array that contains event listeners<br />that are defined by HTMLMediaElement, MediaSource and SourceBuffer.</dd>
+                <dt>autoplay<span class="Arguments__type">boolean</span></dt>
+                <dd>This value should be true in the case of autoplay. The default value is false.</dd>
+              </dl>
+            `
+          }
+        ]}
+      />
+      <CodeViewer title='MediaModule#setup' path='BYXeZb' />
+    </main>
+  );
+};
 
-  render() {
-    return (
-      <main className={Setup.CLASS_NAME}>
-        <section>
-          <div className="component-title">
-            <h1>setup</h1>
-            <p className="returns">Returns : <span>MediaModule</span></p>
-          </div>
-          <hr role="presentation" />
-          <p>
-            This method setups HTMLMediaElement, media formats and event listeners.
-          </p>
-          <table>
-            <caption>Arguments</caption>
-            <thead><tr><th scope="col"></th><th scope="col">Type</th><th scope="col">Description</th></tr></thead>
-            <tbody>
-              <tr>
-                <th scope="row">1st</th>
-                <td><span className="argument-type">Object</span></td>
-                <td>
-                  This argument is plain object that has the following properties.
-                  <dl className="list-marker">
-                    <dt>media<span className="argument-type">HTMLAudioElement</span> or <span className="argument-type">HTMLVideoElement</span></dt>
-                    <dd>This value is the instance of HTMLAudioElement or HTMLVideoElement.</dd>
-                    <dt>formats<span className="argument-type">Array</span></dt>
-                    <dd>This value is the array that contains media formats.</dd>
-                    <dt>listeners<span className="argument-type">Object</span></dt>
-                    <dd>This value is the associative array that contains event listeners that are defined by HTMLMediaElement, MediaSource and SourceBuffer.</dd>
-                    <dt>autoplay<span className="argument-type">boolean</span></dt>
-                    <dd>This value should be true in the case of autoplay. The default value is false.</dd>
-                  </dl>
-                </td>
-              </tr>
-            </tbody>
-          </table>
-        </section>
-        <CodeViewer title='MediaModule#setup' path='BYXeZb' />
-      </main>
-    );
-  }
-}
-
-export default connect()(Setup);
+Setup.TITLE = 'MediaModule#setup';
--- src/components/media/index.js
+++ src/components/media/index.js
@@ -1,5 +1,5 @@
 import { MediaModule } from './MediaModule';
-import Setup from './Setup';
+import { Setup } from './Setup';
 import Ready from './Ready';
 import Start from './Start';
 import Stop from './Stop';
