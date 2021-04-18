0724a39883054ed072aa1e20b31b45abc22f40f9
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-04-18 22:56:00 +0900

    Refactor `StreamModule#start` component by using layout components

--- src/components/stream/Start/index.js
+++ src/components/stream/Start/index.js
@@ -1,48 +1,20 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { Overview } from '../../layouts/Overview';
 import { CodeViewer } from '../../layouts/CodeViewer';
 
-class Start extends React.Component {
-  static TITLE      = 'StreamModule#start';
-  static CLASS_NAME = 'Start';
+const CLASS_NAME = 'StreamModule#Start';
 
-  shouldComponentUpdate() {
-    return false;
-  }
+export const Start = () => {
+  return (
+    <main className={CLASS_NAME}>
+      <Overview
+        title='start'
+        returnTypes={['StreamModule']}
+        description='This method starts streaming.'
+      />
+      <CodeViewer title='StreamModule#start' path='zWNbpP' />
+    </main>
+  );
+};
 
-  render() {
-    return (
-      <main className={Start.CLASS_NAME}>
-        <section>
-          <div className="component-title">
-            <h1>start</h1>
-            <p className="returns">Returns : <span>StreamModule</span></p>
-          </div>
-          <hr role="presentation" />
-          <p>
-            This method starts streaming.
-          </p>
-          <table>
-            <caption>Arguments</caption>
-            <thead><tr><th scope="col"></th><th scope="col">Type</th><th scope="col">Description</th></tr></thead>
-            <tbody>
-              <tr>
-                <th scope="row">1st</th>
-                <td><span className="argument-type">Array</span></td>
-                <td>This value is the array for changing connection.</td>
-              </tr>
-              <tr>
-                <th scope="row">2nd</th>
-                <td><span className="argument-type">Function</span></td>
-                <td>This value is <a href="http://webaudio.github.io/web-audio-api/#the-scriptprocessornode-interface---deprecated" target="_blank" rel="noopener noreferrer nofollow">onaudioprocess</a> event hanlder.</td>
-              </tr>
-            </tbody>
-          </table>
-        </section>
-        <CodeViewer title='StreamModule#start' path='zWNbpP' />
-      </main>
-    );
-  }
-}
-
-export default connect()(Start);
+Start.TITLE = 'StreamModule#start';
--- src/components/stream/index.js
+++ src/components/stream/index.js
@@ -1,7 +1,7 @@
 import { StreamModule } from './StreamModule';
 import { Setup } from './Setup';
 import { Ready } from './Ready';
-import Start from './Start';
+import { Start } from './Start';
 import Stop from './Stop';
 import Param from './Param';
 import Params from './Params';
