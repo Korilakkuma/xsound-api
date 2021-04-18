12ca1e0328d6a724643b21050cb2ab8196007a42
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-04-18 23:12:00 +0900

    Refactor `StreamModule#stop` component by using layout components

--- src/components/stream/Stop/index.js
+++ src/components/stream/Stop/index.js
@@ -1,32 +1,20 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { Overview } from '../../layouts/Overview';
 import { CodeViewer } from '../../layouts/CodeViewer';
 
-class Stop extends React.Component {
-  static TITLE      = 'StreamModule#stop';
-  static CLASS_NAME = 'Stop';
+const CLASS_NAME = 'StreamModule#Stop';
 
-  shouldComponentUpdate() {
-    return false;
-  }
+export const Stop = () => {
+  return (
+    <main className={CLASS_NAME}>
+      <Overview
+        title='stop'
+        returnTypes={['StreamModule']}
+        description='This method stops streaming.'
+      />
+      <CodeViewer title='StreamModule#stop' path='ZxLPmX' />
+    </main>
+  );
+};
 
-  render() {
-    return (
-      <main className={Stop.CLASS_NAME}>
-        <section>
-          <div className="component-title">
-            <h1>stop</h1>
-            <p className="returns">Returns : <span>StreamModule</span></p>
-          </div>
-          <hr role="presentation" />
-          <p>
-            This method stops streaming.
-          </p>
-        </section>
-        <CodeViewer title='StreamModule#stop' path='ZxLPmX' />
-      </main>
-    );
-  }
-}
-
-export default connect()(Stop);
+Stop.TITLE = 'StreamModule#stop';
--- src/components/stream/index.js
+++ src/components/stream/index.js
@@ -2,7 +2,7 @@ import { StreamModule } from './StreamModule';
 import { Setup } from './Setup';
 import { Ready } from './Ready';
 import { Start } from './Start';
-import Stop from './Stop';
+import { Stop } from './Stop';
 import Param from './Param';
 import Params from './Params';
 import ToJSON from './ToJSON';
