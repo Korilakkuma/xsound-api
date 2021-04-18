b06996a13b8e209882785c1cc3b1a9b7e37ec5b3
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-04-18 18:00:00 +0900

    Refactor `StreamModule#setup` component by using layout components

--- src/components/stream/Setup/index.js
+++ src/components/stream/Setup/index.js
@@ -1,53 +1,37 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { Overview } from '../../layouts/Overview';
+import { Arguments } from '../../layouts/Arguments';
 import { CodeViewer } from '../../layouts/CodeViewer';
 
-class Setup extends React.Component {
-  static TITLE      = 'StreamModule#setup';
-  static CLASS_NAME = 'Setup';
+const CLASS_NAME = 'StreamModule#Setup';
 
-  shouldComponentUpdate() {
-    return false;
-  }
+export const Setup = () => {
+  return (
+    <main className={CLASS_NAME}>
+      <Overview
+        title='setup'
+        returnTypes={['StreamModule']}
+        description='This method setups streaming.'
+      />
+      <Arguments
+        rows={[
+          {
+            types      : ['Object'],
+            description: 'This value is to determine constraints for WebRTC.'
+          },
+          {
+            types      : ['Function'],
+            description: 'This function is invoked during streaming.'
+          },
+          {
+            types      : ['Function'],
+            description: 'This function is invoked on error.'
+          }
+        ]}
+      />
+      <CodeViewer title='StreamModule#setup' path='YapbRv' />
+    </main>
+  );
+};
 
-  render() {
-    return (
-      <main className={Setup.CLASS_NAME}>
-        <section>
-          <div className="component-title">
-            <h1>setup</h1>
-            <p className="returns">Returns : <span>StreamModule</span></p>
-          </div>
-          <hr role="presentation" />
-          <p>
-            This method setups streaming.
-          </p>
-          <table>
-            <caption>Arguments</caption>
-            <thead><tr><th scope="col"></th><th scope="col">Type</th><th scope="col">Description</th></tr></thead>
-            <tbody>
-              <tr>
-                <th scope="row">1st</th>
-                <td><span className="argument-type">Object</span></td>
-                <td>This value is to determine constraints for WebRTC.</td>
-              </tr>
-              <tr>
-                <th scope="row">2nd</th>
-                <td><span className="argument-type">Function</span></td>
-                <td>This function is invoked during streaming.</td>
-              </tr>
-              <tr>
-                <th scope="row">3rd</th>
-                <td><span className="argument-type">Function</span></td>
-                <td>This function is invoked on error.</td>
-              </tr>
-            </tbody>
-          </table>
-        </section>
-        <CodeViewer title='StreamModule#setup' path='YapbRv' />
-      </main>
-    );
-  }
-}
-
-export default connect()(Setup);
+Setup.TITLE = 'StreamModule#setup';
