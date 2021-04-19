055b8722f1ebcc2d96126506c0418494d37c01b6
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-04-19 17:02:00 +0900

    Refactor `StreamModule#param` component by using layout components

--- src/components/stream/index.js
+++ src/components/stream/index.js
@@ -3,7 +3,7 @@ import { Setup } from './Setup';
 import { Ready } from './Ready';
 import { Start } from './Start';
 import { Stop } from './Stop';
-import Param from './Param';
+import { Param } from './Param';
 import Params from './Params';
 import ToJSON from './ToJSON';
 import Get from './Get';
