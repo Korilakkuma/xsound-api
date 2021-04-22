7dd380ffc1e33e86f18c29530473ca66fc5c97d3
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-04-22 20:00:00 +0900

    Refactor `MixerModule#mix` component by using layout components

--- src/components/mixer/Mix/index.js
+++ src/components/mixer/Mix/index.js
@@ -1,43 +1,29 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { Overview } from '../../layouts/Overview';
+import { Arguments } from '../../layouts/Arguments';
 import { CodeViewer } from '../../layouts/CodeViewer';
 
-class Mix extends React.Component {
-  static TITLE      = 'MixerModule#mix';
-  static CLASS_NAME = 'Mix';
+const CLASS_NAME = 'MixerModule#Mix';
 
-  shouldComponentUpdate() {
-    return false;
-  }
+export const Mix = () => {
+  return (
+    <main className={CLASS_NAME}>
+      <Overview
+        title='mix'
+        returnTypes={['MixerModule']}
+        description='This method mixes sound sources.'
+      />
+      <Arguments
+        rows={[
+          {
+            types      : ['Array'],
+            description: 'This value is the array that contains sound sources.'
+          }
+        ]}
+      />
+      <CodeViewer title='MixerModule#mix' path='RMLeKm' />
+    </main>
+  );
+};
 
-  render() {
-    return (
-      <main className={Mix.CLASS_NAME}>
-        <section>
-          <div className="component-title">
-            <h1>mix</h1>
-            <p className="returns">Returns : <span>MixerModule</span></p>
-          </div>
-          <hr role="presentation" />
-          <p>
-            This method mixes sound sources.
-          </p>
-          <table>
-            <caption>Arguments</caption>
-            <thead><tr><th scope="col"></th><th scope="col">Type</th><th scope="col">Description</th></tr></thead>
-            <tbody>
-              <tr>
-                <th scope="row">1st</th>
-                <td><span className="argument-type">Array</span></td>
-                <td>This value is the array that contains sound sources.</td>
-              </tr>
-            </tbody>
-          </table>
-        </section>
-        <CodeViewer title='MixerModule#mix' path='RMLeKm' />
-      </main>
-    );
-  }
-}
-
-export default connect()(Mix);
+Mix.TITLE = 'MixerModule#mix';
--- src/components/mixer/index.js
+++ src/components/mixer/index.js
@@ -1,5 +1,5 @@
 import { MixerModule } from './MixerModule';
-import Mix from './Mix';
+import { Mix } from './Mix';
 import Get from './Get';
 
 export {
