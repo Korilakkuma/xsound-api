21dc1497fcd7e4e3a0d03d9820bcc9e08c4cda13
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-04-23 20:00:00 +0900

    Refactor `ProcessorModule#start` component by using layout components

--- src/components/processor/Start/index.js
+++ src/components/processor/Start/index.js
@@ -1,48 +1,33 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { Overview } from '../../layouts/Overview';
+import { Arguments } from '../../layouts/Arguments';
 import { CodeViewer } from '../../layouts/CodeViewer';
 
-class Start extends React.Component {
-  static TITLE      = 'ProcessorModule#start';
-  static CLASS_NAME = 'Start';
+const CLASS_NAME = 'ProcessorModule#Start';
 
-  shouldComponentUpdate() {
-    return false;
-  }
+export const Start = () => {
+  return (
+    <main className={CLASS_NAME}>
+      <Overview
+        title='start'
+        returnTypes={['ProcessorModule']}
+        description='This method starts sound by connecting to AudioDestinationNode.'
+      />
+      <Arguments
+        rows={[
+          {
+            types      : ['Function'],
+            description: 'This value is <a href="https://www.w3.org/TR/webaudio/#scriptprocessornode" target="_blank" rel="noopener noreferrer">onaudioprocess</a> event hanlder.'
+          },
+          {
+            types      : ['Array'],
+            description: 'This value is the array for changing connection.'
+          }
+        ]}
+      />
+      <CodeViewer title='ProcessorModule#start' path='YgjMGz' />
+    </main>
+  );
+};
 
-  render() {
-    return (
-      <main className={Start.CLASS_NAME}>
-        <section>
-          <div className="component-title">
-            <h1>start</h1>
-            <p className="returns">Returns : <span>ProcessorModule</span></p>
-          </div>
-          <hr role="presentation" />
-          <p>
-            This method starts onaudioprocess event in the instance of ScriptProcessorNode.
-          </p>
-          <table>
-            <caption>Arguments</caption>
-            <thead><tr><th scope="col"></th><th scope="col">Type</th><th scope="col">Description</th></tr></thead>
-            <tbody>
-              <tr>
-                <th scope="row">1st</th>
-                <td><span className="argument-type">Function</span></td>
-                <td>This value is <a href="http://webaudio.github.io/web-audio-api/#the-scriptprocessornode-interface---deprecated" target="_blank" rel="noopener noreferrer nofollow">onaudioprocess</a> event hanlder.</td>
-              </tr>
-              <tr>
-                <th scope="row">2nd</th>
-                <td><span className="argument-type">Array</span></td>
-                <td>This value is the array for changing connection.</td>
-              </tr>
-            </tbody>
-          </table>
-        </section>
-        <CodeViewer title='ProcessorModule#start' path='YgjMGz' />
-      </main>
-    );
-  }
-}
-
-export default connect()(Start);
+Start.TITLE = 'ProcessorModule#start';
--- src/components/processor/index.js
+++ src/components/processor/index.js
@@ -1,7 +1,7 @@
 import { ProcessorModule } from './ProcessorModule';
 import { Setup } from './Setup';
 import { Ready } from './Ready';
-import Start from './Start';
+import { Start } from './Start';
 import Stop from './Stop';
 import PostMessage from './PostMessage';
 import OnMessage from './OnMessage';
