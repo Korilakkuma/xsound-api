770d9dfe9a6fcda42a11630e13f2b23699c1ffdd
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-04-23 18:00:00 +0900

    Refactor `ProcessorModule#ready` component by using layout components

--- src/components/processor/Ready/index.js
+++ src/components/processor/Ready/index.js
@@ -1,48 +1,33 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { Overview } from '../../layouts/Overview';
+import { Arguments } from '../../layouts/Arguments';
 import { CodeViewer } from '../../layouts/CodeViewer';
 
-class Ready extends React.Component {
-  static TITLE      = 'ProcessorModule#ready';
-  static CLASS_NAME = 'Ready';
+const CLASS_NAME = 'ProcessorModule#Ready';
 
-  shouldComponentUpdate() {
-    return false;
-  }
+export const Ready = () => {
+  return (
+    <main className={CLASS_NAME}>
+      <Overview
+        title='ready'
+        returnTypes={['Promise']}
+        description='This method adds module for worklet and creates the instance of AudioWorkletNode.'
+      />
+      <Arguments
+        rows={[
+          {
+            types      : ['string'],
+            description: 'This value is the string containing the URL of a JavaScript file with the module to add.'
+          },
+          {
+            types      : ['Request.credentials'],
+            description: 'This value is one of &apos;omit&apos;, &apos;same-origin&apos;, &apos;include&apos;. The default value is &apos;same-origin&apos;.'
+          }
+        ]}
+      />
+      <CodeViewer title='ProcessorModule#ready' path='mdrpwow' />
+    </main>
+  );
+};
 
-  render() {
-    return (
-      <main className={Ready.CLASS_NAME}>
-        <section>
-          <div className="component-title">
-            <h1>ready</h1>
-            <p className="returns">Returns : <span>Promise</span></p>
-          </div>
-          <hr role="presentation" />
-          <p>
-            This method adds module for worklet and creates the instance of <code>AudioWorkletNode</code>.
-          </p>
-          <table>
-            <caption>Arguments</caption>
-            <thead><tr><th scope="col"></th><th scope="col">Type</th><th scope="col">Description</th></tr></thead>
-            <tbody>
-              <tr>
-                <th scope="row">1st</th>
-                <td><span className="argument-type">string</span></td>
-                <td>This value is the string containing the URL of a JavaScript file with the module to add.</td>
-              </tr>
-              <tr>
-                <th scope="row">2nd</th>
-                <td><span className="argument-type">AudioWorkletNodeOptions</span></td>
-                <td>This value is one of &apos;omit&apos;, &apos;same-origin&apos;, &apos;include&apos;. The default value is &apos;same-origin&apos;.</td>
-              </tr>
-            </tbody>
-          </table>
-        </section>
-        <CodeViewer title='ProcessorModule#ready' path='mdrpwow' />
-      </main>
-    );
-  }
-}
-
-export default connect()(Ready);
+Ready.TITLE = 'ProcessorModule#ready';
--- src/components/processor/index.js
+++ src/components/processor/index.js
@@ -1,6 +1,6 @@
 import { ProcessorModule } from './ProcessorModule';
 import { Setup } from './Setup';
-import Ready from './Ready';
+import { Ready } from './Ready';
 import Start from './Start';
 import Stop from './Stop';
 import PostMessage from './PostMessage';
