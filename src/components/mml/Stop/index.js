5caa007b759a9bb851f07ebf227c4dd96c18fddb
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-04-29 18:00:00 +0900

    Refactor `MML#stop` component by using layout components

--- src/components/mml/Stop/index.js
+++ src/components/mml/Stop/index.js
@@ -1,43 +1,20 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { Overview } from '../../layouts/Overview';
 import { CodeViewer } from '../../layouts/CodeViewer';
 
-class Stop extends React.Component {
-  static TITLE      = 'MML#stop';
-  static CLASS_NAME = 'Stop';
+const CLASS_NAME = 'MML#Stop';
 
-  shouldComponentUpdate() {
-    return false;
-  }
+export const Stop = () => {
+  return (
+    <main className={CLASS_NAME}>
+      <Overview
+        title='stop'
+        returnTypes={['MML']}
+        description='This method stops MML.'
+      />
+      <CodeViewer title='MML#stop' path='jzvjPK' />
+    </main>
+  );
+};
 
-  render() {
-    return (
-      <main className={Stop.CLASS_NAME}>
-        <section>
-          <div className="component-title">
-            <h1>stop</h1>
-            <p className="returns">Returns : <span>MML</span></p>
-          </div>
-          <hr role="presentation" />
-          <p>
-            This method stops MML.
-          </p>
-          <table>
-            <caption>Arguments</caption>
-            <thead><tr><th scope="col"></th><th scope="col">Type</th><th scope="col">Description</th></tr></thead>
-            <tbody>
-              <tr>
-                <th scope="row">1st</th>
-                <td><span className="argument-type">Function</span></td>
-                <td>This value is <a href="http://webaudio.github.io/web-audio-api/#the-scriptprocessornode-interface---deprecated" target="_blank" rel="noopener noreferrer nofollow">onaudioprocess</a> event hanlder.</td>
-              </tr>
-            </tbody>
-          </table>
-        </section>
-        <CodeViewer title='MML#stop' path='jzvjPK' />
-      </main>
-    );
-  }
-}
-
-export default connect()(Stop);
+Stop.TITLE = 'MML#stop';
--- src/components/mml/index.js
+++ src/components/mml/index.js
@@ -2,7 +2,7 @@ import { MML } from './MML';
 import { Setup } from './Setup';
 import { Ready } from './Ready';
 import { Start } from './Start';
-import Stop from './Stop';
+import { Stop } from './Stop';
 import Get from './Get';
 import IsSequences from './IsSequences';
 import IsPaused from './IsPaused';
