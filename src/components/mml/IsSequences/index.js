888d5f6fc7169c2f3faf6db10f8aae7bd083aa27
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-05-06 23:09:08 +0900

    Refactor `MML#isSequences` component by using layout components

--- src/components/mml/IsSequences/index.js
+++ src/components/mml/IsSequences/index.js
@@ -1,32 +1,20 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { Overview } from '../../layouts/Overview';
 import { CodeViewer } from '../../layouts/CodeViewer';
 
-class IsSequences extends React.Component {
-  static TITLE      = 'MML#isSequences';
-  static CLASS_NAME = 'IsSequences';
+const CLASS_NAME = 'MML#IsSequences';
 
-  shouldComponentUpdate() {
-    return false;
-  }
+export const IsSequences = () => {
+  return (
+    <main className={CLASS_NAME}>
+      <Overview
+        title='isSequences'
+        returnTypes={['boolean']}
+        description='This method is to check for presence or absence of parsed MML.'
+      />
+      <CodeViewer title='MML#isSequences' path='ZxwmRr' />
+    </main>
+  );
+};
 
-  render() {
-    return (
-      <main className={IsSequences.CLASS_NAME}>
-        <section>
-          <div className="component-title">
-            <h1>isSequences</h1>
-            <p className="returns">Returns : <span>boolean</span></p>
-          </div>
-          <hr role="presentation" />
-          <p>
-            This method is to check for presence or absence of parsed MML.
-          </p>
-        </section>
-        <CodeViewer title='MML#isSequences' path='ZxwmRr' />
-      </main>
-    );
-  }
-}
-
-export default connect()(IsSequences);
+IsSequences.TITLE = 'MML#isSequences';
--- src/components/mml/index.js
+++ src/components/mml/index.js
@@ -4,7 +4,7 @@ import { Ready } from './Ready';
 import { Start } from './Start';
 import { Stop } from './Stop';
 import { Get } from './Get';
-import IsSequences from './IsSequences';
+import { IsSequences } from './IsSequences';
 import IsPaused from './IsPaused';
 import ToABC from './ToABC';
 
