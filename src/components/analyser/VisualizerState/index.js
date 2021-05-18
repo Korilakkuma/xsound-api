89a4b1af10a7d6cad008ff3cf77c8aa3e3dc4402
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-05-18 12:50:29 +0900

    Refactor `Visualizer#state` component by using layout components

--- src/components/analyser/VisualizerState/index.js
+++ src/components/analyser/VisualizerState/index.js
@@ -1,43 +1,29 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { Overview } from '../../layouts/Overview';
+import { Arguments } from '../../layouts/Arguments';
 import { CodeViewer } from '../../layouts/CodeViewer';
 
-class VisualizerState extends React.Component {
-  static TITLE      = 'Visualizer#state';
-  static CLASS_NAME = 'VisualizerState';
+const CLASS_NAME = 'Visualizer#State';
 
-  shouldComponentUpdate() {
-    return false;
-  }
+export const VisualizerState = () => {
+  return (
+    <main className={CLASS_NAME}>
+      <Overview
+        title='state'
+        returnTypes={['boolean', 'Visualizer']}
+        description='This method gets or sets state of analyser.'
+      />
+      <Arguments
+        rows={[
+          {
+            types      : ['boolean'],
+            description: 'This value is to determine analyser state. If this value is undefined, this method is getter.'
+          }
+        ]}
+      />
+      <CodeViewer title='Visualizer#state' path='QrQgJz' />
+    </main>
+  );
+};
 
-  render() {
-    return (
-      <main className={VisualizerState.CLASS_NAME}>
-        <section>
-          <div className="component-title">
-            <h1>Visualizer#state</h1>
-            <p className="returns">Returns : <span>boolean | (TimeOverview | Time | FFT)</span></p>
-          </div>
-          <hr role="presentation" />
-          <p>
-            This method gets or sets state of analyser.
-          </p>
-          <table>
-            <caption>Arguments</caption>
-            <thead><tr><th scope="col"></th><th scope="col">Type</th><th scope="col">Description</th></tr></thead>
-            <tbody>
-              <tr>
-                <th scope="row">1st</th>
-                <td><span className="argument-type">boolean</span> or <span className="argument-type">undefined</span></td>
-                <td>This value is to determine analyser state. If this value is undefined, this method is getter.</td>
-              </tr>
-            </tbody>
-          </table>
-        </section>
-        <CodeViewer title='Visualizer#state' path='QrQgJz' />
-      </main>
-    );
-  }
-}
-
-export default connect()(VisualizerState);
+VisualizerState.TITLE = 'Visualizer#state';
--- src/components/analyser/index.js
+++ src/components/analyser/index.js
@@ -8,7 +8,7 @@ import { Param } from './Param';
 import { Get } from './Get';
 import { VisualizerSetup } from './VisualizerSetup';
 import { VisualizerParam } from './VisualizerParam';
-import VisualizerState from './VisualizerState';
+import { VisualizerState } from './VisualizerState';
 import VisualizerCreate from './VisualizerCreate';
 import TimeOverviewUpdate from './TimeOverviewUpdate';
 import TimeOverviewDrag from './TimeOverviewDrag';
