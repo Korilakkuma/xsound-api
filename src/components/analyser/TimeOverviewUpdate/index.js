a489ba95bdc8220f6ff3b2a0190b81b194d04e64
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-05-19 09:00:43 +0900

    Refactor `TimeOverview#update` component by using layout components

--- src/components/analyser/TimeOverviewUpdate/index.js
+++ src/components/analyser/TimeOverviewUpdate/index.js
@@ -1,43 +1,29 @@
 import React from 'react';
-import { connect } from 'react-redux';
+import { Overview } from '../../layouts/Overview';
+import { Arguments } from '../../layouts/Arguments';
 import { CodeViewer } from '../../layouts/CodeViewer';
 
-class TimeOverviewUpdate extends React.Component {
-  static TITLE      = 'TimeOverview#update';
-  static CLASS_NAME = 'TimeOverviewUpdate';
+const CLASS_NAME = 'TimeOverview#Update';
 
-  shouldComponentUpdate() {
-    return false;
-  }
+export const TimeOverviewUpdate = () => {
+  return (
+    <main className={CLASS_NAME}>
+      <Overview
+        title='update'
+        returnTypes={['TimeOverview']}
+        description='This method draws the current time of audio on Canvas or SVG.'
+      />
+      <Arguments
+        rows={[
+          {
+            types      : ['number'],
+            description: 'This value is the current time of audio.'
+          }
+        ]}
+      />
+      <CodeViewer title='TimeOverview#update' path='NMYgJB' />
+    </main>
+  );
+};
 
-  render() {
-    return (
-      <main className={TimeOverviewUpdate.CLASS_NAME}>
-        <section>
-          <div className="component-title">
-            <h1>TimeOverview#update</h1>
-            <p className="returns">Returns : <span>TimeOverview</span></p>
-          </div>
-          <hr role="presentation" />
-          <p>
-            This method draws the current time of audio on Canvas or SVG.
-          </p>
-          <table>
-            <caption>Arguments</caption>
-            <thead><tr><th scope="col"></th><th scope="col">Type</th><th scope="col">Description</th></tr></thead>
-            <tbody>
-              <tr>
-                <th scope="row">1st</th>
-                <td><span className="argument-type">number</span></td>
-                <td>This value is the current time of audio.</td>
-              </tr>
-            </tbody>
-          </table>
-        </section>
-        <CodeViewer title='TimeOverview#update' path='NMYgJB' />
-      </main>
-    );
-  }
-}
-
-export default connect()(TimeOverviewUpdate);
+TimeOverviewUpdate.TITLE = 'TimeOverview#update';
--- src/components/analyser/index.js
+++ src/components/analyser/index.js
@@ -10,7 +10,7 @@ import { VisualizerSetup } from './VisualizerSetup';
 import { VisualizerParam } from './VisualizerParam';
 import { VisualizerState } from './VisualizerState';
 import { VisualizerCreate } from './VisualizerCreate';
-import TimeOverviewUpdate from './TimeOverviewUpdate';
+import { TimeOverviewUpdate } from './TimeOverviewUpdate';
 import TimeOverviewDrag from './TimeOverviewDrag';
 
 export {
