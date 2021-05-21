22fab73602009e364c63b7955ed6311b96e2416a
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-05-21 09:50:27 +0900

    Refactor `Distortion` component by using layout components

--- src/components/effectors/Distortion/index.js
+++ src/components/effectors/Distortion/index.js
@@ -1,32 +1,20 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import SyntaxHighlighter from 'react-syntax-highlighter';
-import { xcode } from 'react-syntax-highlighter/dist/esm/styles/hljs';
+import { Overview } from '../../layouts/Overview';
+import { Interface } from '../../layouts/Interface';
+import { Parameters } from '../../layouts/Parameters';
 import { CodeViewer } from '../../layouts/CodeViewer';
 
-class Distortion extends React.Component {
-  static TITLE      = 'Distortion';
-  static CLASS_NAME = 'Distortion';
+const CLASS_NAME = 'Distortion';
 
-  shouldComponentUpdate() {
-    return false;
-  }
-
-  render() {
-    return (
-      <main className={Distortion.CLASS_NAME}>
-        <section>
-          <div className="component-title">
-            <h1>distortion</h1>
-            <p className="applicable">Applicable : <span>*</span></p>
-          </div>
-          <hr role="presentation" />
-          <p>
-            This module is Distortion.
-          </p>
-          <section>
-            <h2>Interface</h2>
-            <SyntaxHighlighter language='typescript' style={xcode}>{`type BufferSize = 0 | 256 | 512 | 1024 | 2048 | 4096 | 8192 | 16384;
+export const Distortion = () => {
+  return (
+    <main className={CLASS_NAME}>
+      <Overview
+        title='distortion'
+        applicableModules={['*']}
+        description=''
+      />
+      <Interface interfaceString={`type BufferSize = 0 | 256 | 512 | 1024 | 2048 | 4096 | 8192 | 16384;
 
 type Curve = 'clean' | 'crunch' | 'overdrive' | 'distortion' | 'fuzz';
 
@@ -41,61 +29,56 @@ interface DistortionParams {
 
 interface Distortion {
   constructor(context: AudioContext, bufferSize: BufferSize);
-  param(key: DistortionParams, value?: number): Curve | number | void;
-  params(): DistortionParams;
-  state(isActive?: boolean): boolean | void;
-  toJSON(): string;
+  param(key: string | DistortionParams, value?: number): Curve | number | Distortion;
+  params(void): DistortionParams;
+  state(isActive?: boolean): boolean | Distortion;
+  toJSON(void): string;
   INPUT: GainNode;
   OUTPUT: GainNode;
-}`}</SyntaxHighlighter>
-          </section>
-          <table>
-            <caption>Parameters</caption>
-            <thead><tr><th scope="col"></th><th scope="col">Type</th><th scope="col">Value</th><th scope="col">Default</th></tr></thead>
-            <tbody>
-              <tr>
-                <th scope="row">curve</th>
-                <td><span className="param-type">string</span></td>
-                <td>&apos;clean&apos;, &apos;crunch&apos;, &apos;overdrive&apos;, &apos;distortion&apos;, &apos;fuzz&apos;</td>
-                <td>&apos;clean&apos;</td>
-              </tr>
-              <tr>
-                <th scope="row">samples</th>
-                <td><span className="param-type">number</span></td>
-                <td>value &gt; 0</td>
-                <td>4096</td>
-              </tr>
-              <tr>
-                <th scope="row">amount</th>
-                <td><span className="param-type">number</span></td>
-                <td>0 &lt; value &lt; 1</td>
-                <td>0.5</td>
-              </tr>
-              <tr>
-                <th scope="row">drive</th>
-                <td><span className="param-type">number</span></td>
-                <td>0 &lt;= value &lt;= 100</td>
-                <td>1</td>
-              </tr>
-              <tr>
-                <th scope="row">color</th>
-                <td><span className="param-type">number</span></td>
-                <td>10 &lt;= value &lt;= half the sample-rate</td>
-                <td>350</td>
-              </tr>
-              <tr>
-                <th scope="row">tone</th>
-                <td><span className="param-type">number</span></td>
-                <td>10 &lt;= value &lt;= half the sample-rate</td>
-                <td>350</td>
-              </tr>
-            </tbody>
-          </table>
-        </section>
-        <CodeViewer title='Distortion' path='ELxEqa' />
-      </main>
-    );
-  }
-}
+}`} />
+      <Parameters
+        rows={[
+          {
+            name: 'curve',
+            type: 'string',
+            value: '\'clean\', \'crunch\', \'overdrive\', \'distortion\', \'fuzz\'',
+            defaultValue: '\'clean\''
+          },
+          {
+            name: 'samples',
+            type: 'number',
+            value: 'value < 0',
+            defaultValue: 4096
+          },
+          {
+            name        : 'amount',
+            type        : 'number',
+            value       : '0 < value < 1',
+            defaultValue: 0.5
+          },
+          {
+            name: 'drive',
+            type: 'number',
+            value: '0 <= value <= 100',
+            defaultValue: 1
+          },
+          {
+            name        : 'color',
+            type        : 'number',
+            value       : '10 <= value <= half the sample-rate',
+            defaultValue: 350
+          },
+          {
+            name        : 'tone',
+            type        : 'number',
+            value       : '10 <= value <= half the sample-rate',
+            defaultValue: 350
+          }
+        ]}
+      />
+      <CodeViewer title='Distortion' path='ELxEqa' />
+    </main>
+  );
+};
 
-export default connect()(Distortion);
+Distortion.TITLE = 'Distortion';
--- src/components/effectors/index.js
+++ src/components/effectors/index.js
@@ -2,7 +2,7 @@ import { Autopanner } from './Autopanner';
 import { Chorus } from './Chorus';
 import { Compressor } from './Compressor';
 import { Delay } from './Delay';
-import Distortion from './Distortion';
+import { Distortion } from './Distortion';
 import EnvelopeGenerator from './EnvelopeGenerator';
 import Equalizer from './Equalizer';
 import Filter from './Filter';
