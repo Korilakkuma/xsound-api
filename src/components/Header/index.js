dbc22500ff560a1a8e8f58869751be1ab3c1d3ae
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-06-05 19:25:46 +0900

    Update subtitle

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -17,7 +17,7 @@ export const Header = () => {
       </a>
       <div className={`${CLASS_NAME}__headings`}>
         <h1 className={`${CLASS_NAME}__title`}><Link to='/'>XSound</Link></h1>
-        <h2 className={`${CLASS_NAME}__subtitle`}>Web Audio API Library</h2>
+        <h2 className={`${CLASS_NAME}__subtitle`}>Powerful Audio Features Easily !</h2>
       </div>
     </header>
   );
