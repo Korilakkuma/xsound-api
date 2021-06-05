fb7721b28d963d2fc8f895fe34a87cb7dc2a41b6
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-06-05 19:25:26 +0900

    Update title

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 export const BASS_PATH = '/';  // location.pathname
-export const TITLE = 'XSound - Web Audio API Library for Synthesizer, Effects, Visualization, Multi-Track Recording, Audio Streaming, Visual Audio Sprite ... -';
+export const TITLE = 'XSound - Powerful Audio Features Easily ! -';
