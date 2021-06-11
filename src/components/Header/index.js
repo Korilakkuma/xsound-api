1d3bbf1676a77d0fe2c717666275eb334b23a967
Tomohiro IKEDA <rilakkuma.san.xjapan@gmail.com>
2021-06-11 23:55:41 +0900

    fix: Use CSS  instead of image for GitHub ribbon

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -6,14 +6,8 @@ const CLASS_NAME  = 'Header';
 export const Header = () => {
   return (
     <header className={CLASS_NAME}>
-      <a href="https://github.com/Korilakkuma/XSound" target="_blank" rel="noopener noreferrer">
-        <img
-          className={`${CLASS_NAME}__forkMeOnGitHub`}
-          src="https://s3.amazonaws.com/github/ribbons/forkme_right_gray_6d6d6d.png"
-          alt="Fork me on GitHub"
-          width="149"
-          height="149"
-        />
+      <a href="https://github.com/Korilakkuma/XSound" target="_blank" rel="noopener noreferrer" className={`${CLASS_NAME}__forkMeOnGitHub`}>
+        Fork me on GitHub
       </a>
       <div className={`${CLASS_NAME}__headings`}>
         <h1 className={`${CLASS_NAME}__title`}><Link to='/'>XSound</Link></h1>
